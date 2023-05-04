d277a0dee1dc3aac512e82395496d17bba441418
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-04 15:30:12 +0200

    add valueFilm

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,30 @@
 <script>
+  import {store} from '../assets/js/store';
   export default {
-   name: 'SearchBar' 
+   name: 'SearchBar',
+   
+   data() {
+    return {
+      store
+    }
+   },
+
+   methods: {
+    log() {
+      console.log(this.store.valueFilm);
+    }
+   }
   }
 </script>
 
 <template>
   <div class="search-bar">
-    <input type="text" placeholder="Insert name film">
+    <input
+      type="text"
+      placeholder="Insert name film"
+      v-model="store.valueFilm"
+      @keyup.enter="log()"
+    >
     <button>Search</button>
   </div>
 </template>
