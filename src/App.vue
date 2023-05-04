c313ceb1f1674e78aeba9454943f12798358765e
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-04 13:59:05 +0200

    add getApi()

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script>
   import SearchBar from './components/SearchBar.vue';
   import Main from './components/Main.vue';
+import axios from 'axios';
   export default {
    name: 'App',
    
    components: {
     SearchBar,
     Main
+   },
+
+   methods: {
+    getApi() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=b35e5259fc80aa97841e519ada2e3d64&language=it-IT&query=matrix')
+      .then(result => {
+        console.log(result.data);
+      })
+    }
+   },
+
+   mounted(){
+    this.getApi()
    }
   }
 </script>
