0cbdf068009dfa85c9cee7e8af1a49d3d34d630b
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-04 12:57:58 +0200

    add search-bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,7 +5,10 @@
 </script>
 
 <template>
-  SEARCHBAR
+  <div class="search-bar">
+    <input type="text" placeholder="Insert name film">
+    <button>Search</button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
